806a34572ecbfc20d07b20e45bc8d736f561f698
Joshua Grosbard <joshgrosbard@gmail.com>
2012-07-26 19:09:37 -0400

    updated ulysses & get from sinatra

--- ulysses_bot.rb
+++ ulysses_bot.rb
@@ -1,121 +1,122 @@
-<<<<<<< HEAD
-source_text = <<EOF
-They came down the steps from Leahy's terrace prudently, Frauenzimmer: and down the shelving shore flabbily, their splayed feet sinking in the silted sand. Like me, like Algy, coming down to our mighty mother. Number one swung lourdily her midwife's bag, the other's gamp poked in the beach. From the liberties, out for the day. Mrs Florence MacCabe, relict of the late Patk MacCabe, deeply lamented, of Bride Street. One of her sisterhood lugged me squealing into life. Creation from nothing. What has she in the bag? A misbirth with a trailing navelcord, hushed in ruddy wool. The cords of all link back, strandentwining cable of all flesh. That is why mystic monks. Will you be as gods? Gaze in your omphalos. Hello! Kinch here. Put me on to Edenville. Aleph, alpha: nought, nought, one.
-Spouse and helpmate of Adam Kadmon: Heva, naked Eve. She had no navel. Gaze. Belly without blemish, bulging big, a buckler of taut vellum, no, whiteheaped corn, orient and immortal, standing from everlasting to everlasting. Womb of sin.
-Wombed in sin darkness I was too, made not begotten. By them, the man with my voice and my eyes and a ghostwoman with ashes on her breath. They clasped and sundered, did the coupler's will. From before the ages He willed me and now may not will me away or ever. A lex eterna stays about Him. Is that then the divine substance wherein Father and Son are consubstantial? Where is poor dear Arius to try conclusions? Warring his life long upon the contransmagnificandjewbangtantiality. Illstarred heresiarch' In a Greek watercloset he breathed his last: euthanasia. With beaded mitre and with crozier, stalled upon his throne, widower of a widowed see, with upstiffed omophorion, with clotted hinderparts.
-Airs romped round him, nipping and eager airs. They are coming, waves. The whitemaned seahorses, champing, brightwindbridled, the steeds of Mananaan.
-I mustn't forget his letter for the press. And after? The Ship, half twelve. By the way go easy with that money like a good young imbecile.
-Yes, I must.
-His pace slackened. Here. Am I going to aunt Sara's or not? My consubstantial father's voice. Did you see anything of your artist brother Stephen lately? No? Sure he's not down in Strasburg terrace with his aunt Sally? Couldn't he fly a bit higher than that, eh? And and and and tell us, Stephen, how is uncle Si? O, weeping God, the things I married into! De boys up in de hayloft. The drunken little costdrawer and his brother, the cornet player. Highly respectable gondoliers! And skeweyed Walter sirring his father, no less! Sir. Yes, sir. No, sir. Jesus wept: and no wonder, by Christ!
-EOF
 
-source_text_words = source_text.split(' ') #split source_text and store in new array source_text_words
-word_pairs_and_probabilities = {} #initalize new variable word_pairs_and_probabilities as a hash
+require 'sinatra'
 
-source_text_words.each_with_index do |word, index|
-  hash_key = "#{word} #{source_text_words[index + 1]}" 
-  hash_value = source_text_words[index + 2] 
-  if word_pairs_and_probabilities[hash_key] 
-    word_pairs_and_probabilities[hash_key] << hash_value 
-  else
-    word_pairs_and_probabilities[hash_key] = [hash_value]
-      
-      
-=======
-class LitBot
-  def eat_file(file_name)
-    file = File.open(file_name)
-    @source_text = file.read
-    file.close
-  end
-
-  def speak_first_10_words
-    puts @source_text.split(' ').first(10)
-  end
-
-  def digest_file
-    source_text_words = @source_text.split(' ')
-    @word_pairs_and_probabilities = {}
-
-    source_text_words.each_with_index do |word, index|
-      hash_key = "#{word} #{source_text_words[index + 1]}"
-      hash_value = source_text_words[index + 2]
-      if @word_pairs_and_probabilities[hash_key]
-        @word_pairs_and_probabilities[hash_key] << hash_value
-      else
-        @word_pairs_and_probabilities[hash_key] = [hash_value]
-      end
+get '/hi' do
+    "Hello World!"
     end
-  end
 
-  def speak
-    puts @word_pairs_and_probabilities.first
->>>>>>> upstream/master
-  end
-end
-
-bender = LitBot.new
-bender.eat_file("huckle.txt")
-# bender.speak_first_10_words # an example call
-bender.digest_file
-bender.speak
-
-ulysses_bot = LitBot.new
-ulysses_bot.eat_file("ulysses.txt")
-# ulysses.speak_first_10_words # an example call
-ulysses_bot.digest_file
-ulysses_bot.speak
-
-# BONUS HW
-hybrid_bot = ulysses_bot + bender
-hybrid_bot.speak
-
-# ulysses = File.open("ulysses.txt")
-# source_text = ulysses.read
-# ulysses.close
-
-# source_text_words = source_text.split(' ')
-# word_pairs_and_probabilities = {}
-
-# source_text_words.each_with_index do |word, index|
-  # hash_key = "#{word} #{source_text_words[index + 1]}"
-  # hash_value = source_text_words[index + 2]
-  # if word_pairs_and_probabilities[hash_key]
+class LitBot
+    def eat_file(file_name)
+        file = File.open(file_name)
+        self.source_text = file.read
+        file.close
+    end
+    
+    def speak_first_10_words
+        puts source_text.split(' ').first(10)
+    end
+    
+    def digest_file
+        @source_text_words = source_text.split(' ')
+        @word_pairs_and_probabilities = {}
+        
+        @source_text_words.each_with_index do |word, index|
+            hash_key = "#{word} #{@source_text_words[index + 1]}"
+            hash_value = @source_text_words[index + 2]
+            if @word_pairs_and_probabilities[hash_key]
+                @word_pairs_and_probabilities[hash_key] << hash_value
+                else
+                @word_pairs_and_probabilities[hash_key] = [hash_value]
+            end
+        end
+    end
+    
+    def speak
+        output_text = generate_random_seed_pair
+        story = 0
+        while story < 35 do
+            word_pair = output_text.last(2).join(' ')
+            next_word = @word_pairs_and_probabilities[word_pair].sample unless @word_pairs_and_probabilities[word_pair].nil?
+            output_text << next_word
+            
+            if next_word && (next_word.include?(".") || next_word.include?("?"))
+                story += 1
+            end
+        end
+            output_text.join(' ')
+        end
+        
+        def generate_random_seed_pair
+            trial_pair = @word_pairs_and_probabilities.keys.sample
+            until trial_pair[0] == trial_pair[0].upcase do
+                trial_pair = @word_pairs_and_probabilities.keys.sample
+            end
+                trial_pair.split(' ')
+            end
+            
+            def +(other_bot)
+            shiny_new_litbot = LitBot.new(source_text + other_bot.source_text)
+        end
+        
+        #getter
+        def source_text
+            @source_text
+        end
+        
+        #setter
+        def source_text=(text)
+            @source_text = text
+        end
+        
+        def initialize(text="Any string")
+            self.source_text = text
+        end
+    end
+    
+    bender = LitBot.new
+    bender.eat_file("huckle.txt")
+    bender.digest_file
+    # puts "Bender says, \"#{bender.speak}\""
+    
+    ulysses_bot = LitBot.new
+    ulysses_bot.eat_file("ulysses.txt")
+    ulysses_bot.digest_file
+    # puts "Ulysses says, \"#{ulysses_bot.speak}\""
+    
+    # BONUS HW
+    hybrid_bot = ulysses_bot + bender
+    hybrid_bot.digest_file
+    # puts hybrid_bot
+    puts "Hybrid Bot says \"#{hybrid_bot.speak}\""
+    
+    # ulysses = File.open("ulysses.txt")
+    # source_text = ulysses.read
+    # ulysses.close
+    
+    # source_text_words = source_text.split(' ')
+    # word_pairs_and_probabilities = {}
+    
+    # source_text_words.each_with_index do |word, index|
+    # hash_key = "#{word} #{source_text_words[index + 1]}"
+    # hash_value = source_text_words[index + 2]
+    # if word_pairs_and_probabilities[hash_key]
     # word_pairs_and_probabilities[hash_key] << hash_value
-  # else
+    # else
     # word_pairs_and_probabilities[hash_key] = [hash_value]
-  # end
-# end
-
-# # puts word_pairs_and_probabilities
-# output_text = ['They', 'came']
-
-# story = 0
-# while story < 35 do
-  # word_pair = output_text.last(2).join(' ')
-  # next_word = word_pairs_and_probabilities[word_pair].sample unless word_pairs_and_probabilities[word_pair].nil?
-  # output_text << next_word
-
-  # if next_word && (next_word.include?(".") || next_word.include?("?"))
+    # end
+    # end
+    
+    # # puts word_pairs_and_probabilities
+    # output_text = ['They', 'came']
+    
+    # story = 0
+    # while story < 35 do
+    # word_pair = output_text.last(2).join(' ')
+    # next_word = word_pairs_and_probabilities[word_pair].sample unless word_pairs_and_probabilities[word_pair].nil?
+    # output_text << next_word
+    
+    # if next_word && (next_word.include?(".") || next_word.include?("?"))
     # story += 1
-  # end
-# end
-# puts output_text.join(' ')
-
-
-
-
-
-
-
-<<<<<<< HEAD
-hash_key_to_pass = 
-probable_following_words = word_pairs_and_probabilities[hash_key]
-
-
-
-
-# simple_hash = {}
-# simple_hash["they came"] =["down"]
-=======
->>>>>>> upstream/master
+    # end
+    # end
+    # puts output_text.join(' ')
